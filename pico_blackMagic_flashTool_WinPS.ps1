03dc0706f72af74d45975b40e43604c35b55a7cd
VanessaPrankl-usepat <110904384+VanessaPrankl-usepat@users.noreply.github.com>
2022-12-20 13:14:55 +0100

    Added flash tool for Windows
    Execute within PowerShell

---
+++ pico_blackMagic_flashTool_WinPS.ps1
@@ -0,0 +1,25 @@
+﻿<# powershell script for resetting and flashing rp2040 autonomously #>
+
+<# check if rapsi has mounted as serial-device #>
+echo "waiting"
+while((mode $args /status) -eq $False){
+   Start-Sleep -m 100
+}
+
+<# reset pico over baud rate change to 1200 baud #>
+echo "resetting pico"
+mode $args baud=12
+
+<# check if rapsi has mounted as USB-device #>
+while((Test-Path -Path F:\) -eq $False){
+    Start-Sleep -m 500 <# sleep for 500 ms #>
+}
+
+<# copy flash-file to raspi #>
+echo "initiate copying"
+Copy-Item -Filter *.uf2 -Path 'C:\Users\Vanessa\Documents\flashTool' -Recurse -Destination 'F:\RPI-RP2'
+
+echo "done"
+
+
+

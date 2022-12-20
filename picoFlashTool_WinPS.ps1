4283ca57e1c2fd3a74fcd24c695571738f4ea287
VanessaPrankl-usepat <110904384+VanessaPrankl-usepat@users.noreply.github.com>
2022-12-20 14:43:59 +0100

    Updated Windows Flash tool
    Added some error handling

--- picoFlashTool_WinPS.ps1
+++ picoFlashTool_WinPS.ps1
@@ -1,25 +1,50 @@
 ﻿<# powershell script for resetting and flashing rp2040 autonomously #>
 
 <# check if rapsi has mounted as serial-device #>
-echo "waiting"
+echo "Wait for serial device on port $args"
+$i = 0
 while((mode $args /status) -eq $False){
-   Start-Sleep -m 100
+   Start-Sleep -m 250
+   $i += 1
+   
+   <# Abort script if no serial device could be found #>
+   if($i -ge 12){
+        echo "No device found on port $args - Aborting process."
+        Exit 1 
+   }
 }
 
 <# reset pico over baud rate change to 1200 baud #>
 echo "resetting pico"
-mode $args baud=12
+mode $args baud=12 parity=n data=8 stop=1
 
 <# check if rapsi has mounted as USB-device #>
+$i = 0
 while((Test-Path -Path F:\) -eq $False){
-    Start-Sleep -m 500 <# sleep for 500 ms #>
+    echo "."
+    Start-Sleep -m 250 <# sleep for 250 ms #>
+    $i += 1
+
+    <# Abort script if no usb device could be found #>
+   if($i -ge 12){
+        echo "No device found on drive F:\ - Aborting process."
+        Exit 1 
+   }
 }
 
 <# copy flash-file to raspi #>
 echo "initiate copying"
-Copy-Item -Filter *.uf2 -Path 'C:\Users\Vanessa\Documents\flashTool' -Recurse -Destination 'F:\RPI-RP2'
+Copy-Item -Filter *.uf2 -Path '.\' -Recurse -Destination 'F:\RPI-RP2'
 
-echo "done"
+Start-Sleep -m 1000 <# give pico some time to unmount #>
+
+<# check if rapsi is still mounted as USB-device #>
+if((Test-Path -Path F:\) -eq $True){
+    echo "pico was reset but couldn't be flashed - please drag and drop .uf2-file manually"
+}
+else{ 
+    echo "done"
+}
 
 
 
